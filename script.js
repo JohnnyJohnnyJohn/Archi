490a60f5c37c2e98cf9337cf9aafffa04366636f
JohnnyJohnnyJohn <fethisedjai@gmail.com>
2022-11-17 09:18:49 +0100

    new

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+if (window.innerWidth < 960){
+    document.getElementsByTagName("header")[0].style.backgroundColor = "#EFE8E1"
+}
+
 window.onscroll = function () {
     // We add pageYOffset for compatibility with IE.
+
     if (window.scrollY >= 120) {
-        document.getElementsByTagName("header")[0].style.backgroundColor = "#ffffff75";
+        document.getElementsByTagName("header")[0].style.backgroundColor = "#ffffff90";
     } else {
         document.getElementsByTagName("header")[0].style.backgroundColor = "transparent"
+    }
+};
+
+window.addEventListener('resize', function(event) {
+    if (window.innerWidth < 960){
+        document.getElementsByTagName("header")[0].style.backgroundColor = "#EFE8E1"
+    } else {
+        if (window.scrollY >= 120) {
+            document.getElementsByTagName("header")[0].style.backgroundColor = "#ffffff90";
+        } else {
+            document.getElementsByTagName("header")[0].style.backgroundColor = "transparent"
+        }
+    }
+}, true);
+
+document.getElementById("menu").onclick = function(){
+    for (let index = 0; index < document.getElementsByTagName("nav").length; index++) {
+        document.getElementsByTagName("nav")[index].style.display = "block";
     }
 };
